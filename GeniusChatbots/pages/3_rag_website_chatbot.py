f80d2734e312bede7b2e4b245a3047d513ec3e2f
everyfine <zuluck2004@icloud.com>
2024-05-25 10:38:32 +0800

    Finish the stream response in rag website chatbot

--- GeniusChatbots/pages/3_rag_website_chatbot.py
+++ GeniusChatbots/pages/3_rag_website_chatbot.py
@@ -20,7 +20,6 @@
 -------------------------------------------------
 """
 
-
 __author__ = 'EveryFine'
 
 import os
@@ -41,6 +40,8 @@ import streamlit as st
 from llm_clients.openai_rag_client import OpenAIRagClient
 
 st.set_page_config(page_title="RAG Website Chatbot", page_icon="🦀")
+
+
 def get_vectorstore_from_url(url):
     # get the text in document form
     loader = WebBaseLoader(url)
@@ -75,15 +76,6 @@ def create_chatbot():
             st.session_state.chat_history = [
                 AIMessage(content="Hello, I am a bot. How can I help you?"),
             ]
-        if "vector_store" not in st.session_state:
-            st.session_state.vector_store = get_vectorstore_from_url(website_url)
-        user_query = st.chat_input("Type your message here...")
-        if user_query is not None and user_query != "":
-            rag_client = OpenAIRagClient(model_name, openai_api_key)
-            response = rag_client.get_invoke_response(user_query, st.session_state.chat_history,
-                                                      st.session_state.vector_store)
-            st.session_state.chat_history.append(HumanMessage(content=user_query))
-            st.session_state.chat_history.append(AIMessage(content=response))
         for message in st.session_state.chat_history:
             if isinstance(message, AIMessage):
                 with st.chat_message("AI"):
@@ -91,6 +83,30 @@ def create_chatbot():
             elif isinstance(message, HumanMessage):
                 with st.chat_message("Human"):
                     st.write(message.content)
+        if "vector_store" not in st.session_state:
+            st.session_state.vector_store = get_vectorstore_from_url(website_url)
+        user_query = st.chat_input("Type your message here...")
+        if user_query is not None and user_query != "":
+            st.session_state.chat_history.append(HumanMessage(content=user_query))
+            with st.chat_message("Human"):
+                st.markdown(user_query)
+
+            with st.chat_message("AI"):
+                rag_client = OpenAIRagClient(model_name, openai_api_key)
+                # response = rag_client.get_invoke_response(user_query, st.session_state.chat_history,
+                #                                       st.session_state.vector_store)
+                stream = rag_client.get_stream_response(user_query, st.session_state.chat_history,
+                                                        st.session_state.vector_store)
+
+                response_container = st.empty()
+                response_text = ""
+                for chunk in stream:
+                    if answer_chunk := chunk.get("answer"):
+                        response_text += answer_chunk
+                        # 实时更新Streamlit界面上的内容
+                        response_container.markdown(response_text)
+
+                st.session_state.chat_history.append(AIMessage(content=response_text))
 
 
 login('.streamlit/config.yaml')
